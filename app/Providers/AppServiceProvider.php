b7b4022dfb45d724c7c163c06acc6d96bcc42a79
George Miracle <georgemiracle2019@gmail.com>
2026-05-04 14:12:24 +0800

    back again

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,8 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        URL::forceScheme('https');
+        if (request()->server('HTTP_X_FORWARDED_PROTO') === 'https') {
+            URL::forceScheme('https');
+        }
     }
 }
